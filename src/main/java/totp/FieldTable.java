2ed2c113be32389e45e2d60e101beb05685b1819
Phan Ngọc Cường <cuongharryit@gmail.com>
2024-10-08 00:55:54 +0700

    Feat: totp v2

---
+++ src/main/java/totp/FieldTable.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package totp;
+
+/**
+ *
+ * @author AnhEmPhanMem
+ */
+public class FieldTable {
+
+    public final static int USERNAME = 0;
+    public final static int PASSWORD = 1;
+    public final static int SECRET_KEY = 2;
+    public final static int TOKEN = 3;
+    public final static int TIMER = 4;
+    
+    public static void main(String[] args) {
+    }
+}
